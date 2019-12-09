109986703e59016a2fd48580b388fe428e8989fe
Lutta <denilsonwash@gmail.com>
2019-12-09 13:39:57 +0300

    Added main file

---
+++ flaskPost.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+import json
+
+app = Flask(__name__)
+
+@app.route('/',methods=["POST","GET"])
+def postServer():
+    if request.method=="POST":
+        password=request.get_json()
+                
+        json_string = json.dumps(password)
+        print(json_string)
+
+        return "hello luttas you posted{}".format(password)
+    
+    return "<h1>YOU PERFORMED A GET REQUEST SO YOU GET THE GET PAGE</h1>"
