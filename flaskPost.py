5b43fcd19b5a7ab27adf4385cc0db5e82a26635e
Lutta <denilsonwash@gmail.com>
2019-12-10 11:21:13 +0300

    g

--- flaskPost.py
+++ flaskPost.py
@@ -8,12 +8,17 @@ app = Flask(__name__)
 def postServer():
     if request.method=="POST":
         password=request.get_json()
-                
-        json_string = json.dumps(password["key"])
-        print("denny initialized "+ str(json_string))
+        try:
+            json_string = json.dumps(password["key"])
+            print("denny initialized "+ str(json_string))
+            return "hello luttas you posted{}".format(password)
+        except:
+            return "Key not in your post request"
+            
+        
 
 
-        return "hello luttas you posted{}".format(password)
+        
     
     return "<h1>YOU PERFORMED A GET REQUEST SO YOU GET THE GET PAGE</h1>"
 
